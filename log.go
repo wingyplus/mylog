26431626c0b78702376c9369f587cd7218139230
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2015-12-15 16:41:38 +0700

    rename Logger.allowedLevels to Logger.allowedSeverities

--- log.go
+++ log.go
@@ -23,27 +23,27 @@ const (
 )
 
 type Logger struct {
-	writer        io.Writer
-	allowedLevels Severity
+	writer            io.Writer
+	allowedSeverities Severity
 
 	mu sync.Mutex
 }
 
 var logger = &Logger{
-	writer:        bufio.NewWriter(os.Stdout),
-	allowedLevels: ALL,
+	writer:            bufio.NewWriter(os.Stdout),
+	allowedSeverities: ALL,
 }
 
 func (logger *Logger) SetOutput(w io.Writer) {
 	logger.writer = w
 }
 
-func (logger *Logger) SetAllowedSeverities(lvls Severity) {
-	logger.allowedLevels = lvls
+func (logger *Logger) SetAllowedSeverities(severities Severity) {
+	logger.allowedSeverities = severities
 }
 
 func (logger *Logger) Write(lv Severity, v ...interface{}) {
-	if (lv & logger.allowedLevels) != 0 {
+	if (lv & logger.allowedSeverities) != 0 {
 		logger.mu.Lock()
 		defer logger.mu.Unlock()
 
